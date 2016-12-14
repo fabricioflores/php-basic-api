c942fdfd54c7d37f1d8872ccb4ccc6b0a2e689d0
Fabricio Flores <fabrifloresg@gmail.com>
2016-12-14 09:06:08 +0100

    Add get single result

--- src/routes_results.php
+++ src/routes_results.php
@@ -49,3 +49,50 @@ $app->get(
         return $response->withJson(array('results' => $results));
     }
 )->setName('miw_cget_results');
+
+/**
+ * Summary: Returns a result based on a single ID
+ * Notes: Returns the result identified by &#x60;resultId&#x60;.
+ *
+ * @SWG\Get(
+ *     method      = "GET",
+ *     path        = "/results/{resultId}",
+ *     tags        = { "Results" },
+ *     summary     = "Returns a result based on a single ID",
+ *     description = "Returns the result identified by `resultId`.",
+ *     operationId = "miw_get_results",
+ *     parameters  = {
+ *          { "$ref" = "#/parameters/resultId" }
+ *     },
+ *     @SWG\Response(
+ *          response    = 200,
+ *          description = "Result",
+ *          schema      = { "$ref": "#/definitions/Result" }
+ *     ),
+ *     @SWG\Response(
+ *          response    = 404,
+ *          description = "Result id. not found",
+ *          schema      = { "$ref": "#/definitions/Message" }
+ *     )
+ * )
+ */
+$app->get(
+    '/results/{id:[0-9]+}',
+    function ($request, $response, $args) {
+        $this->logger->info('GET \'/results/' . $args['id'] . '\'');
+        $result = getEntityManager()
+            ->getRepository('MiW16\Results\Entity\Result')
+            ->findOneById($args['id']);
+
+        if (empty($result)) {  // 404 - User id. not found
+            $newResponse = $response->withStatus(404);
+            $datos = array(
+                'code' => 404,
+                'message' => 'Result not found'
+            );
+            return $this->renderer->render($newResponse, 'message.phtml', $datos);
+        }
+
+        return $response->withJson($result);
+    }
+)->setName('miw_get_results');
